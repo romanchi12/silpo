024797c5ed553c61c71809a340259402407f9700
Roman Malyarchuk <romanchi12@users.noreply.github.com>
2017-08-31 19:17:28 +0300

    Add files via upload

--- src/main/java/org/romanchi/App.java
+++ src/main/java/org/romanchi/App.java
@@ -2,6 +2,7 @@ package org.romanchi;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ import java.io.IOException;
  *  31.08.2017
  */
 public class App {
-    public static void main( String[] args ) throws IOException {
+    public static void printSilpoGoods() throws IOException {
         Document pageDocument = Jsoup.connect("http://silpo.ua/ua/actions/priceoftheweek/?PAGEN_1=1&").get();
         int pages = pageDocument.select(".page div").size();
         for(int page = 1; page <= pages; page++){
@@ -25,5 +26,21 @@ public class App {
                 System.out.println(String.format("%s Акційна ціна %s грн. Нормальна ціна %s грн.", goodName, goodNewPrice, goodOldPrice));
             });
         }
+    }
+    public static void printAtbGoods() throws IOException {
+        Document doc = Jsoup.connect("http://www.atbmarket.com/hot/akcii/economy").get();
+        Elements goods = doc.select(".promo_list li");
+        goods.forEach(good -> {
+            String goodName = good.select(".promo_info_text").text();
+            String kop = good.select(".promo_price span").get(0).text();
+            good.select(".promo_price span").forEach(span -> span.html(""));
+            String goodNewPrice = good.select(".promo_price").text() + "." + kop;
+            String goodOldPrice = good.select(".promo_old_price").text();
+            System.out.println(String.format("%s Акційна ціна %s грн. Нормальна ціна %s грн.", goodName, goodNewPrice, goodOldPrice));
+        });
+    }
+    public static void main( String[] args ) throws IOException {
+        App.printAtbGoods();
+        App.printSilpoGoods();
     }
 }
